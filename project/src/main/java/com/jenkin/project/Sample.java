5e2f7188f9ea9d1e046169132b8bb97a53e9f672
sakunthala-lash <sakunthala@lashron.com>
2025-02-01 17:22:58 +0800

    fix: fix-spelling-mistake

--- project/src/main/java/com/jenkin/project/Sample.java
+++ project/src/main/java/com/jenkin/project/Sample.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Sample{
 
 	@GetMapping("/home")
-	public ResponseEntity<Void> getHomee() {
+	public ResponseEntity<Void> getHome() {
 		return ResponseEntity.status(HttpStatus.OK).build();
 	}
 }
