a4648ad669532832ad46cdf974d49ed5763f8c6a
sakunthala-lash <sakunthala@lashron.com>
2025-02-01 09:49:36 +0800

    feat/added-initial-code

---
+++ project/src/main/java/com/jenkin/project/Sample.java
@@ -0,0 +1,17 @@
+package com.jenkin.project;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/sample")
+public class Sample{
+
+	@GetMapping("/home")
+	public ResponseEntity<Void> getDashboard() {
+		return ResponseEntity.status(HttpStatus.OK).build();
+	}
+}
