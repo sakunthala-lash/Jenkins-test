de8f9c47e09270d10fc493412f16af548272c936
sakunthala-lash <sakunthala@lashron.com>
2025-02-10 15:50:33 +0800

    feat:Add test case

---
+++ project/src/test/java/com/jenkin/project/SampleTest.java
@@ -0,0 +1,14 @@
+package com.jenkin.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(SampleTest.class)
+public class SampleTest {
+
+    @Test
+    public void testGetHomeSuccess() throws Exception {
+       
+    }
+    
+}
