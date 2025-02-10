afbf2f3b6e292e7b887ca351067424c097a7371f
sakunthala-lash <sakunthala@lashron.com>
2025-02-10 18:05:48 +0800

    fix: correct 3

--- project/src/test/java/com/jenkin/project/SampleTest.java
+++ project/src/test/java/com/jenkin/project/SampleTest.java
@@ -1,15 +1,10 @@
 package com.jenkin.project;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(SampleTest.class)
 public class SampleTest {
-
-    @Autowired
-    private MockMvc mockMvc;
 
     @Test
     public void testGetHomeSuccess() throws Exception {
