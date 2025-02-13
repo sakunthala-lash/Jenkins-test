3a10e29f2bb6cb65c037747aa15c2cfce0014b35
sakunthala-lash <sakunthala@lashron.com>
2025-02-13 13:20:18 +0800

    feat/1

--- project/src/main/java/com/jenkin/project/Sample.java
+++ project/src/main/java/com/jenkin/project/Sample.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Sample{
 
 	@GetMapping("/home")
-	public ResponseEntity<Void> getHome() {
+	public ResponseEntity<Void> getHomes() {
 		return ResponseEntity.status(HttpStatus.OK).build();
 	}
 }
